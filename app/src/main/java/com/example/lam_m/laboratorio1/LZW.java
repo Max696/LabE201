a769de199294c276ca2e4e27a7a929854e5ce3cb
Max696 <maxferdiaz@gmail.com>
2018-09-26 19:31:41 -0600

    this
    Co-Authored-By: carloslam <carloslam@users.noreply.github.com>

---
+++ app/src/main/java/com/example/lam_m/laboratorio1/LZW.java
@@ -0,0 +1,247 @@
+package com.example.lam_m.laboratorio1;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class LZW {
+    String textoComprimido;
+
+    LZW(String xD){
+
+        String resultado = "";
+        int z = 1;
+        Map<Integer, String> map = new TreeMap<>();
+
+        List<String> a = new ArrayList();
+
+        for (int i = 0; i < xD.length(); i++) {
+            char q = xD.charAt(i);
+            String s = Character.toString(q);
+            if(a.isEmpty()){
+                a.add(s);
+            }else{
+                if(!a.contains(s)){
+                    a.add(s);
+                }
+            }
+        }
+        Collections.sort(a);
+        for (int i = 0; i < a.size(); i++) {
+            map.put(z,a.get(i));
+            z++;
+        }
+        int contador = 0;
+        Boolean aux = false;
+        textoComprimido = "";
+        for (int i = 0; i < xD.length(); i++) {
+            String s = Character.toString(xD.charAt(i));
+            int value = 0;
+            if(map.containsValue(s)){
+                while(map.containsValue(s)){
+                    if(i+contador+1 < xD.length()){
+                        contador++;
+                        s = s + xD.charAt(i+contador);
+                    }else{
+                        //DeberÃ­a ser el fin del string o terminando
+                        aux = true;
+                        break;
+                    }
+                }
+                if(aux){
+                    //Aqui va el codigo en caso de que le falten letras
+                    for(Map.Entry<Integer, String> entry : map.entrySet()){
+                        if(entry.getValue().equals(s)){
+                            if(entry.getKey().longValue() > 9){
+                                //Dejar espacio
+                                textoComprimido = textoComprimido +  "(" + entry.getKey() + ")";
+                            }else{
+                                textoComprimido = textoComprimido + entry.getKey();
+                            }
+                        }
+                    }
+                    break;
+                }
+
+                for(Map.Entry<Integer, String> entry : map.entrySet()){
+                    if(entry.getValue().equals(s.substring(0,s.length()-1))){
+                        if(entry.getKey().longValue() > 9){
+                            //Dejar espacio
+                            textoComprimido = textoComprimido +  "(" + entry.getKey() + ")";
+                        }else{
+                            textoComprimido = textoComprimido + entry.getKey();
+                        }
+                    }
+                }
+                map.put(z, s);
+                //Se debe ver cuantas letras se comio
+                if(contador > 1){
+                    i = i + contador - 1;
+                }
+                contador = 0;
+                z++;
+            }
+        }
+        String prueba1 = toComprimir(textoComprimido);//texto comprimido
+        String arbolCOMP = treeCompress(a.toString());//Quedan numeros, se convierten a ascii
+        arbolCOMP = toComprimir(arbolCOMP);//Arbol comprimido
+        textoComprimido = prueba1 + "â– " + arbolCOMP;//archivo final
+    }
+    public String deCompressArchivo(String textoComprimido){
+        Map<Integer, String> khe = new HashMap<>();
+        int cont = 1;
+        String qwerty = textoComprimido.substring(0, textoComprimido.indexOf("â– "));
+        String arbolinho = textoComprimido.substring(textoComprimido.indexOf("â– ") + 1, textoComprimido.length());
+        String prueba2 = deCompress(qwerty);
+        String arbolDescomprimido = treeDecompress(arbolinho);
+
+        arbolinho = arbolDescomprimido.substring(1, arbolDescomprimido.length()-1);
+        String[] hellow = arbolinho.split("\\, ");
+
+        for (String hellow1 : hellow) {
+            khe.put(cont, hellow1);
+            cont++;
+        }
+
+        String tD = "";
+        String auxi = "";
+        int uP = 1;
+        int contador = 2;
+
+        for (int i = 1; i < prueba2.length(); i++) {
+            if (i == 1) {
+                if(Character.toString(prueba2.charAt(1-1)).equals("(")){//Cuando el primer caracter se trata de un parentesis
+                    while(prueba2.charAt(i+contador) != ')'){
+                        contador++;
+                    }
+                    String s = prueba2.substring(i, i+contador); //Obtenemos el numero
+                    tD = khe.get(Integer.valueOf(s)); //Descomprimimos
+                    i = i + contador + 1; //Validamos
+                    contador = 2; //Reseteamos variable
+
+                }else{//Cuando no es un parentesis
+                    int p = Integer.valueOf(Character.toString(prueba2.charAt(1-1)));
+                    tD = khe.get(p);
+                }
+                if(Character.toString(prueba2.charAt(i)).equals("(")){//Cuando el segundo numero es mayor a 9
+                    while(prueba2.charAt(i+contador) != ')'){
+                        contador++;
+                    }
+                    String s = prueba2.substring(i+1, i+contador); //Obtenemos el numero
+                    tD = tD + khe.get(Integer.valueOf(s)); //Descomprimimos
+                    i = i + contador; //Validamos
+                    contador = 2; //Reseteamos variable
+                }else{//Cuando es un numero de 0-9
+                    tD = tD + khe.get(Integer.valueOf(Character.toString(prueba2.charAt(i))));
+                }
+                khe.put(cont, tD);
+                cont++;
+                auxi = Character.toString(tD.charAt(tD.length() - 1));
+            }else{
+                //Se biene lo chido bro xDxD
+                if(Character.toString(prueba2.charAt(i)).equals("(")){
+                    //Debemos tomar N numeros
+                    while(prueba2.charAt(i+contador) != ')'){
+                        contador++;
+                    }
+
+                    String s = prueba2.substring(i+1, i+contador); //Obtenemos el numero
+                    tD = tD + khe.get(Integer.valueOf(s)); //Descomprimimos
+                    i = i + contador; //Validamos
+                    contador = 2; //Reseteamos variable
+                }else{
+                    //Se trata de un numero
+                    int p = Integer.valueOf(Character.toString(prueba2.charAt(i)));//Obtenemos el numero
+                    tD = tD + khe.get(p);//Descomprimimos
+                }
+                uP++;
+                auxi = auxi + tD.charAt(uP);
+                while(khe.containsValue(auxi)){
+                    uP++;
+                    auxi = auxi + tD.charAt(uP);
+                }
+                khe.put(cont,auxi);//AÃ±adimos al mapa
+                cont++;
+                auxi = Character.toString(auxi.charAt(auxi.length()-1));//Tomamos la ultima letra
+            }
+        }
+        return tD;
+    }
+    public String compre(){
+        return textoComprimido;
+    }
+    private String treeCompress(String texto) {
+        String resultante = "";
+        for (int i = 0; i < texto.length(); i++) {
+            char character = texto.charAt(i);
+            int ascii = (int)character;
+            resultante = resultante + ascii;
+        }
+        resultante = resultante.substring(2, resultante.length()-2);
+        String[] valores = resultante.split("4432");
+        resultante = "";
+        for (int i = 0; i < valores.length; i++) {
+
+            int numero = Integer.valueOf(valores[i]);
+            char c = (char)numero;
+            if(i ==0){//Se debe crear desde cero xD
+                resultante = "[" + Character.toString(c) + ",";
+            }else if(i == valores.length-1){//Se trata del final
+                resultante = resultante + " " + Character.toString(c) + "]";
+            }else{//Letras enmedio
+                resultante = resultante + " " + Character.toString(c) + ",";
+            }
+        }
+        //String[]valores =
+        return resultante;
+    }
+
+    private String treeDecompress(String arbol) {
+        String resultante = "";
+        for (int i = 0; i < arbol.length(); i++) {
+            char character = arbol.charAt(i); // This gives the character 'a'
+            int ascii = (int) character;
+            resultante = resultante + ascii;
+        }
+        return resultante;
+    }
+
+    private String toComprimir(String textoComprimido){
+        String compre = "";
+        int numero = 0;
+        int contador = 2;
+        for (int i = 0; i < textoComprimido.length(); i++) {
+            if(Character.toString(textoComprimido.charAt(i)).equals("(")){
+                while(textoComprimido.charAt(i+contador) != ')'){
+                    contador++;
+                }
+                numero = Integer.valueOf(textoComprimido.substring(i+1, i+contador));
+                i = i+contador;
+            }else{
+                numero = Integer.valueOf(Character.toString(textoComprimido.charAt(i)));
+            }
+            compre = compre + ((char)numero);
+            contador = 2;
+        }
+        return compre;
+    }
+
+    private String deCompress(String textoComprimido){
+        String textoOriginal = "";
+        for (int i = 0; i < textoComprimido.length(); i++) {
+            int valor = (int)textoComprimido.charAt(i);
+            if(valor > 9){
+                textoOriginal = textoOriginal + "(" + valor + ")";
+            }else{
+                textoOriginal = textoOriginal + valor;
+            }
+        }
+        return textoOriginal;
+    }
+
+
+}
--- app/src/main/java/com/example/lam_m/laboratorio1/MainActivity.java
+++ app/src/main/java/com/example/lam_m/laboratorio1/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.lam_m.laboratorio1;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AppCompatActivity;
@@ -44,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txt_out;
     EditText name,nameunz;
     Huffman hf;
+    LZW lzw;
     String codificacion;
     String totalName="";
     Switch hfOrLzw;
@@ -84,26 +91,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-      /*  btn_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String filename =name.getText().toString();
-                String filename2 = nameunz.getText().toString();
-                hf.descromprimir(hf.metodoFinal());
-
-                try {
-                    saveTextAsFile(filename,filename2,hf.decompress(),hf.metodoFinal());
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });*/
-
       btn_save.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
@@ -127,80 +114,81 @@ public class MainActivity extends AppCompatActivity {
         reader.close();
         return stringBuilder.toString();
     }
-    public void performFileSearch()
-        {
+    public void performFileSearch()       {
         Intent inten = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         inten.addCategory(Intent.CATEGORY_OPENABLE);
         inten.setType("text/*");
         startActivityForResult(inten,READ_REQUEST_CODE);
     }
-
-    public void performFolderSearch()
-    {
-        Intent inten = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-        inten.addCategory(Intent.CATEGORY_OPENABLE);
-        inten.setType("text/*");
-        startActivityForResult(inten,READ_REQUEST_CODE);
-    }
-
-
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            boolean hol = hfOrLzw.isChecked();
-            String asda = "";
+        boolean hol = hfOrLzw.isChecked();
+        if (data != null) {
+            Uri uri = data.getData();
+            totalName = uri.getLastPathSegment();
+            if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+                String s = "";
 
 
-            if (data != null) {
-                Uri uri = data.getData();
-                totalName = uri.getLastPathSegment();
-                Toast.makeText(this, uri.getPath(), Toast.LENGTH_LONG).show();
+
 
                 try {
-                    readText(uri);
-                    String s = readText(uri);
+                    s=readText(uri);
+
+
 
                     if (!hol) {
                         hf = new Huffman(s);
                         codificacion = hf.cifrado1();
                     } else {
-
+                        lzw = new LZW(s);
+                        codificacion = lzw.textoComprimido;
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        }
-        if (resultCode == Activity.RESULT_OK && requestCode == RQS_OPEN_DOCUMENT_TREE) {
-            Uri uriTree = data.getData();
-            Uri uri;
 
-            String path = "";
-            DocumentFile documentFile = DocumentFile.fromTreeUri(this, uriTree);
+            if (resultCode == Activity.RESULT_OK && requestCode == RQS_OPEN_DOCUMENT_TREE) {
+                Uri uriTree = data.getData();
+                String s = "";
+                try {
+                    s = readText(uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            path = uriTree.getLastPathSegment();
-            // Toast.makeText(this, path,Toast.LENGTH_SHORT).show();
-            String filename = name.getText().toString();
-            String filename2 = nameunz.getText().toString();
-            hf.descromprimir(hf.metodoFinal());
 
-            try {
-                saveTextAsFile(filename, filename2, hf.decompress(), hf.metodoFinal(), path
-                );
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+
+                String path = "";
+                DocumentFile documentFile = DocumentFile.fromTreeUri(this, uriTree);
+                uriTree = documentFile.getUri();
+                Uri docUri = DocumentsContract.buildDocumentUriUsingTree(uriTree,
+                        DocumentsContract.getTreeDocumentId(uriTree));
+                path = getPath(this, docUri);
+                String filename = name.getText().toString();
+                String filename2 = nameunz.getText().toString();
+
+
+                try {
+                    if (!hol) {
+                        hf =new Huffman(s);
+                        codificacion = hf.cifrado1();
+                        saveTextAsFile(filename, filename2,hf.decompress(),hf.metodoFinal(), path);
+                        Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+                    } else {
+                        lzw = new LZW(s);
+                        saveTextAsFile(filename, filename2,lzw.deCompressArchivo(lzw.textoComprimido),lzw.textoComprimido, path);
+                        Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+
+                }
+
             }
 
-
-    }
-
-
-
-
-
-
+        }
     }
 
     public void irCompresion(View view){
@@ -211,6 +199,8 @@ public class MainActivity extends AppCompatActivity {
     private void saveTextAsFile(String filename , String filename2,String content2, String content ,String path) throws FileNotFoundException {
         String fileName = filename+".huff";
         String fileName2 = filename2+".txt";
+
+
         File file = new File(path,fileName);
         File file2 = new File(path,fileName2);
         FileOutputStream fos = null;
@@ -256,5 +246,114 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+    }
+
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 }
